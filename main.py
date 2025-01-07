9e2555f3dff877ea765f8581e9274ddc3f8f43fd
Rovin-karki <142002565+Rovin-karki@users.noreply.github.com>
2025-01-07 19:28:35 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,58 @@
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.actor.Actor import Actor
+from panda3d.core import Point3, Vec3
+import sys
 
-class MyApp(App):
-    def build(self):
-        self.label = Label(text="Hello, Kivy!")
-        
-        button = Button(text="Press Me")
-        button.bind(on_press=self.on_button_press)
-        
-        layout = BoxLayout(orientation='vertical')
-        layout.add_widget(self.label)
-        layout.add_widget(button)
-        
-        return layout
-    
-    def on_button_press(self, instance):
-        self.label.text = "Button Pressed!"
+class MyGame(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
 
-if __name__ == '__main__':
-    MyApp().run()
+        # Disable the default mouse camera control
+        self.disableMouse()
+
+        # Load the environment model
+        self.scene = self.loader.loadModel("models/environment")
+        # Reparent the model to render
+        self.scene.reparentTo(self.render)
+        # Apply scale and position transforms
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
+        # Add a spinning teapot
+        self.teapot = self.loader.loadModel("models/teapot")
+        self.teapot.reparentTo(self.render)
+        self.teapot.setPos(0, 5, 0)
+        self.teapot.setScale(1, 1, 1)
+
+        # Set up the camera
+        self.camera.setPos(0, -20, 4)
+        self.camera.lookAt(Point3(0, 0, 0))
+
+        # Add a task to rotate the teapot
+        self.taskMgr.add(self.spinTeapotTask, "SpinTeapotTask")
+
+        # Add keyboard controls
+        self.accept("escape", sys.exit)
+        self.accept("arrow_left", self.moveTeapot, [-1, 0, 0])
+        self.accept("arrow_right", self.moveTeapot, [1, 0, 0])
+        self.accept("arrow_up", self.moveTeapot, [0, 1, 0])
+        self.accept("arrow_down", self.moveTeapot, [0, -1, 0])
+
+    def spinTeapotTask(self, task):
+        """Rotate the teapot each frame."""
+        angleDegrees = task.time * 60.0
+        self.teapot.setHpr(angleDegrees, 0, 0)
+        return Task.cont
+
+    def moveTeapot(self, x, y, z):
+        """Move the teapot based on keyboard input."""
+        self.teapot.setPos(
+            self.teapot.getX() + x,
+            self.teapot.getY() + y,
+            self.teapot.getZ() + z
+        )
+
+# Create and run the game
+game = MyGame()
+game.run()
